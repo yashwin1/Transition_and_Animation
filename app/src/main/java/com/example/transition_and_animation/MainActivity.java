9415ff6c8e0c525d430c73859ae2a873a843b384
Yashwin Agrawal <yashwin18205@iiitd.ac.in>
2020-09-04 20:09:45 +0530

    first commit

---
+++ app/src/main/java/com/example/transition_and_animation/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.transition_and_animation;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.transition.TransitionManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewGroup yashwinLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        yashwinLayout = (ViewGroup) findViewById(R.id.yashwinLayout);
+
+        yashwinLayout.setOnTouchListener(
+                new RelativeLayout.OnTouchListener(){
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        moveButton();
+                        return true;
+                    }
+                }
+        );
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+    public void moveButton(){
+        View yashwinButton = findViewById(R.id.yashwinButton);
+
+        TransitionManager.beginDelayedTransition(yashwinLayout);
+
+//      Change the position of button
+        RelativeLayout.LayoutParams positionRules = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        positionRules.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+        positionRules.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+        yashwinButton.setLayoutParams(positionRules);
+
+//      Change the size of button
+        ViewGroup.LayoutParams sizeRules = yashwinButton.getLayoutParams();
+        sizeRules.width = 450;
+        sizeRules.height = 300;
+        yashwinButton.setLayoutParams(sizeRules);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
